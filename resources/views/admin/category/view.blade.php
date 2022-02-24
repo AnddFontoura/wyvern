325ec46bc8f94c741fee6fbe1dbc3c84465f34c2
André Fontoura <andd.fontoura@hotmail.com>
2022-02-24 13:56:47 -0300

    Melhorando tradução
    Mais ajustes em categoria

--- resources/views/admin/category/view.blade.php
+++ resources/views/admin/category/view.blade.php
@@ -4,8 +4,8 @@
     <div class='container-fluid'>
         <div class='row'>
             <div class='col-12 text-right mb-3'>
-                <a href="{{ url('admin/category/create/'.$category->id) }}" class="btn btn-success"> {{ __('basic.category.edit') }} </a>
-                <a href="{{ url('admin/category') }}" class="btn btn-primary"> {{ __('basic.category.list') }} </a>
+                <a href="{{ url('admin/category/create/'.$category->id) }}" class="btn btn-success"> {{ __('basic.form.edit') }} {{ __('category.commons.name') }} </a>
+                <a href="{{ url('admin/category') }}" class="btn btn-primary"> {{ __('basic.form.list') }} {{ __('category.commons.name') }} </a>
             </div>
         </div>
 
@@ -13,12 +13,12 @@
             <div class="card">
                 <div class="card-body">
                     <div class="form-group">
-                        <label> {{ __('basic.category.form.name') }} </label>
+                        <label> {{ __('category.commons.name') }} </label>
                         <p> {{ $category->name }} <p>
                     </div>
 
                     <div class="form-group">
-                        <label> {{ __('basic.category.form.description') }} </label>
+                        <label> {{ __('category.form.description') }} </label>
                         <p> {!! $category->description !!} </p>
                     </div>
                 </div>
